4990b11c427b53f6cbfad5103e03715c37f64ef0
unknown <chris.a.desilva@gmail.com>
2018-12-09 10:07:14 -0600

    completed js functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,23 @@
-//Strike out todos by clicking
-
-$("li").on("click", function(){
+//Strike out individual todos by clicking
+//listener added to entire ul parent so that new lis can be targeted 
+$("ul").on("click", "li", function(){
    $(this).toggleClass("completed");
 });
-
-$("span").on("click", function(e){
+//fades out and removes completed todos
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(function(){
         $(this).remove();
     });
+    //prevent bubbling up to parent elements
     e.stopPropagation();
+});
+//adds todo lis to ul
+$("input[type='text']").on("keypress", function(e){
+    //listening for enter (13) keypress
+    if(e.which === 13){
+        let todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    };
 });
 
